8cce4bd5970e139e5ddf93cf43e873cfb318658d
Denis Nazarenko <293728den@gmail.com>
2024-02-21 00:14:59 -0500

    eletive iframe extension

--- cypress/e2e/iframe.cy.ts
+++ cypress/e2e/iframe.cy.ts
@@ -20,3 +20,19 @@ describe("IFRAMEAPP", () => {
     IFrameAppPage.getIframe()
   });
 });
+describe.only('IFRAME WITH PLUGIN', () => {
+  beforeEach(() => {
+    cy.visit(`${Cypress.env('herokuapp')}/iframe`);
+  });
+  it('test iFrame', () => {
+    cy.frameLoaded('#mce_0_ifr')
+    cy.iframe('#mce_0_ifr').then((iframe)=>{
+      cy.wrap(iframe).type('{selectAll}{del}').type('Hello World!')
+      cy.wrap(iframe).should('have.text', 'Hello World!')
+      cy.wrap(iframe).clear().type('Cypress')
+      cy.then(()=>{
+        expect(iframe.text()).to.equal('Cypress')
+      })
+    })
+  });
+});
