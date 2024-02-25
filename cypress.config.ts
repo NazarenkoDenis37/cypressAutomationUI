daa5d494c65dc2d2d9cf56fa3afd7ef2228954cc
Denis Nazarenko <293728den@gmail.com>
2024-02-25 12:34:01 -0500

    lesson with excel

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "cypress";
 
+const xlsx = require("node-xlsx").default;
+const fs = require("fs"); // for file
+const path = require("path")
 require("dotenv").config();
 
 export default defineConfig({
@@ -9,6 +12,19 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       require("cypress-mochawesome-reporter/plugin")(on); //reporter
       // implement node event listeners here
+      // reading excel document from fixture
+      on("task", {
+        parseXlsx({ filePath }) {
+          return new Promise((resolve, reject) => {
+            try {
+              const jsonData = xlsx.parse(fs.readFileSync(filePath));
+              resolve(jsonData);
+            } catch (e) {
+              reject(e);
+            }
+          });
+        },
+      });
     },
     env: {
       stage: "https://stage.pasv.us",
